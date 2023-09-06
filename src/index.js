d3217256d06f625504c6725a203ac51411b8f513
Kyicodes <140958930+Kyicodes@users.noreply.github.com>
2023-09-05 23:01:21 -0700

    Integrated moment.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert("Welcome");
+updateTime();
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector("#la-date");
+  let losAngelesTimeElement = document.querySelector("#la-time");
+  let losAngelesTime = moment().tz("UTC America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("LL");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  // Sydney
+  let SydneyElement = document.querySelector("#sydney");
+  let SydneyDateElement = document.querySelector("#sydney-date");
+  let SydneyTimeElement = document.querySelector("#sydney-time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  SydneyDateElement.innerHTML = sydneyTime.format("LL");
+  SydneyTimeElement.innerHTML = sydneyTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+setInterval(updateTime, 1000);
